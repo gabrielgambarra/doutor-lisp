327d56f9d85ff55a150b5902c21442fc9551a9c0
Gabriel <gabriel.gambarra@tonicapp.com>
2024-04-18 09:15:56 +0100

    Refactor file paths and update Lisp execution command in PHP files

--- assets/php_css_lisp/redireciona.php
+++ assets/php_css_lisp/redireciona.php
@@ -1,58 +1,60 @@
-<?php                                                                                                   # Início do php
-        $redireciona = file_get_contents("pergunta.txt");                                               # $redireciona recebe a conteúdo do arquivo
-        if($redireciona == 1){                                                                          # Início do if que verifica se o arquivo é igual a 6
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/semdst.html"</script>');                      # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 2){                                                                          # Início do if que verifica se o arquivo é igual a 5
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/doido.html"</script>');                       # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 3){                                                                          # Início do if que verifica se o arquivo é igual a 4
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/hpv.html"</script>');                         # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 4){                                                                          # Início do if que verifica se o arquivo é igual a 11
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/donovanose.html"</script>');                  # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 5){                                                                          # Início do if que verifica se o arquivo é igual a 10
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/linfogranuloma.html"</script>');              # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 6){                                                                          # Início do if que verifica se o arquivo é igual a 9
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/herpesgenital.html"</script>');               # Script que redireciona para a página da doença                                                                                    # Fim do if
-        }                                                                                               # Fim do if
-        if($redireciona == 7){                                                                          # Início do if que verifica se o arquivo é igual a 8
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/cancromole.html"</script>');                  # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 8){                                                                          # Início do if que verifica se o arquivo é igual a 7
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/sifilis.html"</script>');                     # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 9){                                                                          # Início do if que verifica se o arquivo é igual a 1
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/micoplasma.html"</script>');                  # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 10){                                                                         # Início do if que verifica se o arquivo é igual a 3
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/tricomoniase.html"</script>');                # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-        if($redireciona == 11){                                                                         # Início do if que verifica se o arquivo é igual a 2
-                $pergnova = fopen("pergunta.txt","w");                                                  # $pergnova recebe o arquivo para escrita
-                fwrite($pergnova,"Corrimento pelo canal vaginal ou vontade de urinar constante?");      # Escreve no arquivo a pergunta inicial
-                exit('<script>location.href = "../doencas/gonorreiaclamidia.html"</script>');           # Script que redireciona para a página da doença
-        }                                                                                               # Fim do if
-?>                                                                                                      <!-- Fim do php --> 
+<?php
+$redireciona = file_get_contents("pergunta.txt");
+switch ($redireciona) {
+        case 1:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/semdst.html"</script>');
+                break;
+        case 2:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/doido.html"</script>');
+                break;
+        case 3:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/hpv.html"</script>');
+                break;
+        case 4:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/donovanose.html"</script>');
+                break;
+        case 5:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/linfogranuloma.html"</script>');
+                break;
+        case 6:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/herpesgenital.html"</script>');
+                break;
+        case 7:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/cancromole.html"</script>');
+                break;
+        case 8:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/sifilis.html"</script>');
+                break;
+        case 9:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/micoplasma.html"</script>');
+                break;
+        case 10:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/tricomoniase.html"</script>');
+                break;
+        case 11:
+                $pergnova = fopen("pergunta.txt", "w");
+                fwrite($pergnova, "Corrimento pelo canal vaginal ou vontade de urinar constante?");
+                exit('<script>location.href = "../doencas/gonorreiaclamidia.html"</script>');
+                break;
+}
+?> <!-- Fim do php -->
